840fb7160480813a2efd31eb6f6f901536f12309
Jason Lui <lui.jason10@gmail.com>
2022-08-18 11:03:27 -0400

    Completed the Decryption of RailsFence

--- src/transposition.py
+++ src/transposition.py
@@ -1,10 +1,18 @@
 ### RAIL FENCE
+"""
+Encrypts plain text into chiper text by using the rail fence algo
+pt - Plain text
+key - number of rails desired
+asList- Return the chiper text as a list of rails if true, otherwise as a string
+
+returns - A list of rails or a string of the chiper text
+"""
 def railFenceEn(pt, key, asList):
     rails = [""] * key
     down = True
     curRail = 0
 
-    for i in range(0, len(pt)):
+    for i in range(len(pt)):
         rails[curRail] += pt[i]        
 
         if down:
@@ -21,9 +29,66 @@ def railFenceEn(pt, key, asList):
     if asList:
         return rails
     return ''.join(rails)
-        
 
-def railFenceDe(ct, key):
+"""
+Encrypts the chiper text into plain text by using the rail fence algo
+ct - chiper Text
+key - number of rails desired
+
+returns - The plain text of the chiper text
+"""
+def railFenceDe(ct, key, asList):
+    rails = [""] * key
+    lengths = [0]*key
+    down = True
+    curRail = 0
+
+    #Get the length of each rail
+    for i in range(len(ct)):
+        lengths[curRail] = lengths[curRail] + 1
+
+        if down:
+            curRail = curRail + 1
+            if curRail >= key:
+                down = False
+                curRail = key - 2
+        else:
+            curRail = curRail - 1
+            if curRail < 0:
+                down = True
+                curRail = 1
+    
+    curRail = 0
+    down = True
+
+    currSeek = 0
+
+    #Get the text for each rail
+    for i in range(key):
+        rails[i] = ct[currSeek: currSeek+lengths[i]]
+        currSeek += lengths[i]
+
+    plainText = ""
+    railPos = [0] * key
+
+    #Dechiper
+    for i in range(len(ct)):
+        plainText = plainText + rails[curRail][railPos[curRail]]
+
+        railPos[curRail] += 1
+
+        if down:
+            curRail = curRail + 1
+            if curRail >= key:
+                down = False
+                curRail = key - 2
+        else:
+            curRail = curRail - 1
+            if curRail < 0:
+                down = True
+                curRail = 1       
+
+    print(plainText)
     return None
-
+        
 ### RAIL FENCE
--- src/transpositionTests.py
+++ src/transpositionTests.py
@@ -8,6 +8,12 @@ class railFence(unittest.TestCase):
         self.assertEqual(transposition.railFenceEn("1234567890", 4, True), ["17","268","359","40"])
         self.assertEqual(transposition.railFenceEn("1234567890", 5, True), ["19","280","37","46","5"])
 
+    def testDe(self):
+        self.assertEqual(transposition.railFenceDe("1357924680", 2),"1234567890")
+        self.assertEqual(transposition.railFenceDe("1592468037", 3), "1234567890")
+        self.assertEqual(transposition.railFenceDe("1726835940", 4), "1234567890")
+        self.assertEqual(transposition.railFenceDe("1928037465", 5), "1234567890")        
+
 
 if __name__ == "__main__":
     unittest.main()
